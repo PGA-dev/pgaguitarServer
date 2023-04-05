6f0463b53847579183f8c00ccb5e0f1be6e2618d
PGA-dev <paulgadam@gmail.com>
2023-04-05 01:05:02 -0600

    guitarstat update

--- routes/guitarRouter.js
+++ routes/guitarRouter.js
@@ -3,7 +3,10 @@ const Guitar = require('../models/guitar');
 
 const guitarRouter = express.Router();
 
-//Basic campsite routing
+//not finished -- must update for guitarstats before -- may merge guitar and guitardetail for one big http nightmare
+
+
+//guitar routing for Guitar Directory Page
 guitarRouter.route('/')
 .get((req, res, next) => {
     Guitar.find()
@@ -27,7 +30,7 @@ guitarRouter.route('/')
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /guitar');
-})//will be updated for support
+})
 .delete((req, res, next) => {
     Guitar.deleteMany()
     .then(response => {
@@ -38,7 +41,7 @@ guitarRouter.route('/')
     .catch(err => next(err));
 });
 
-//campsite:Id routing
+//guitar Id for Guitar Detail Page
 guitarRouter.route('/:guitarId')
 .get((req, res, next) => {
     Guitar.findById(req.params.guitarId)
@@ -73,6 +76,80 @@ guitarRouter.route('/:guitarId')
     })
     .catch(err => next(err));
 });
+
+//guitarstat
+guitarRouter.route('/:guitarId/guitarstat')
+.get((req, res, next) => {
+    Guitar.findById(req.params.guitarId)
+    .then(guitar => {
+        if (guitar) {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(guitar.guitarstat);
+        } else {
+            err = new Error(`Guitar ${req.params.guitarId} not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.post((req, res, next) => {
+    Guitar.findById(req.params.guitarId)
+    .then(guitar => {
+        if (guitar) {
+            guitar.guitarstat.push(req.body);
+            guitar.save()
+            .then(guitar => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(guitar);
+            })
+            .catch(err => next(err));
+        } else {
+            err = new Error(`guitar ${req.params.guitarId} data not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /guitar/${req.params.guitarId}/stats`);
+})
+.delete((req, res, next) => {
+    Guitar.findById(req.params.guitarId)
+    .then(guitar => {
+        if (guitar) {
+            for (let i = (guitar.guitarstat.length-1); i >= 0; i--) {
+                guitar.guitarstat.id(guitar.guitarstat[i]._id).remove();
+            }
+            guitar.save()
+            .then(guitar => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(guitar);
+            })
+            .catch(err => next(err));
+        } else {
+            err = new Error(`guitar ${req.params.guitarId} data not found`);
+            err.status = 404;
+            return next(err);
+        }
+    })
+    .catch(err => next(err));
+});
+
+guitarRouter.route('/:guitarId/guitarstat/guitarstat2')
+
+guitarRouter.route('/:guitarId/guitarstat/guitarstat2/guitarstat3')
+
+guitarRouter.route('/:guitarId/guitarstat/guitarstat2/guitarstat3/guitarstat4')
+
+guitarRouter.route('/:guitarId/guitarstat/guitarstat2/guitarstat3/guitarstat4/guitarstat5')
+
+
 
 
 module.exports = guitarRouter;
